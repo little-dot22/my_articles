c50c732cc5af8148a5925944630d7e5fc582981a
Kongby <22260086@zju.edu.cn>
2022-11-28 15:47:03 +0800

    11.28

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,10 +1,6 @@
-
-
-// 组件渲染
-function App () {
-  return (
-    <div className="App">
-    </div>
-  )
-}
-export default App
+import React from "react";
+import HeaderBar from './Header/HeaderBar';
+import './App.css';
+import WeatherModel from './WeatherModel/WeatherModel';
+import MaskView from './WeatherModel/MaskView';
+import WeatherList from './WeatherList/WeatherList';
---
+++ weather/src/Header/HeaderBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Logo from '../asset/logo.svg';
+import SearchBar from './SearchBar';
+import './HeaderBar.css'
+
+class HeaderBar extends React.Component {
+  render() {
+    return (
+      <div className="HeaderBarContainer">
+
+        <div className="HeaderBarLeft">
+          <img src="" alt="" className="HeaderBarLogo" />
+        </div>
+
+      </div>
+    )
+  }
+}
